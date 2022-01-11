afd0c7a4ed2b19d1b034447fc61f7a1503382e45
coding-bug <swl4988@qq.com>
2022-01-11 19:22:06 +0800

    111111

--- admin/src/main/java/cn/bug/admin/web/controller/TestController.java
+++ admin/src/main/java/cn/bug/admin/web/controller/TestController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController extends BaseController {
-
     @Autowired
     private SysUserService sysUserService;
 
--- common/src/main/java/cn/bug/common/controller/BaseController.java
+++ common/src/main/java/cn/bug/common/controller/BaseController.java
@@ -2,6 +2,7 @@ package cn.bug.common.controller;
 
 import cn.bug.common.core.redis.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -10,8 +11,9 @@ import javax.servlet.http.HttpServletRequest;
  * @description
  * @createDate 2022-01-04 13:11
  */
+
 public class BaseController {
-    @Autowired
+    @Autowired(required = false)
     protected HttpServletRequest request;
 
 
